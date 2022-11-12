62b316f6f5bcb72de008679661351f78de1ef29d
prathyusha chamarthy <prathyucp.2021@gmail.com>
2022-11-12 16:49:20 -0500

    checkins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TotalExpenseComponent } from './components/total-expense/total-expense.component';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TotalExpenseComponent,
+    ExpenseListComponent,
+    ExpenseFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
